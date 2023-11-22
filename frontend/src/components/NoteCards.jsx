1730d24a680c6ba9f146f44020c45082e73258c0
Jim Martinez <iamdeterminedhm18@gmail.com>
2023-11-22 20:12:13 +0800

    delete function

--- frontend/src/components/NoteCards.jsx
+++ frontend/src/components/NoteCards.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+import { useSnackbar } from 'notistack';
+import { Link } from 'react-router-dom';
 
 const NoteCards = ({ notes }) => {
+  const { enqueueSnackbar } = useSnackbar();
+  const handleDeleteNote = (id) => {
+    axios
+      .delete(`http://localhost:5000/notes/${id}`)
+      .then(() => {
+        enqueueSnackbar('Note deleted', { variant: 'success' });
+      })
+      .catch((error) => {
+        enqueueSnackbar('Error', { variant: 'error' });
+        console.log(error);
+      });
+  };
+
   return (
     <div className="notecards-container">
       {notes.map((item) => (
         <div className="card" key={item._id}>
           <div className="card-content">
+            <div className="divXmark">
+              <FontAwesomeIcon
+                onClick={() => handleDeleteNote(item._id)}
+                className="xmark-icon"
+                icon={faXmark}
+              />
+            </div>
             <p className="note-title">{item.title}</p>
             <p className="note-content">{item.text}</p>
           </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SnackbarProvider>
-      <App />
-    </SnackbarProvider>
+    <BrowserRouter>
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );

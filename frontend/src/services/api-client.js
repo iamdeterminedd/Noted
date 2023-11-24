e95ff5599dde809910797d72d3636746b5da340c
Jim Martinez <iamdeterminedhm18@gmail.com>
2023-11-24 14:13:50 +0800

    fix incorrect api url

--- frontend/src/services/api-client.js
+++ frontend/src/services/api-client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://noted-api-d555.onrender.com/notes',
+  baseURL: 'https://noted-api-d555.onrender.com',
   // baseURL: 'http://localhost:5000',
 });
